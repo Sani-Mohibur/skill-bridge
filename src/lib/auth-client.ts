a80125a0abaee64467f74a714cda9cbcc1ee262b
Mohibur Rahman <mohiburrahmansani@gmail.com>
2026-06-21 20:41:00 +0600

    fix: update baseURL in authClient to use production API endpoint

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -2,7 +2,7 @@ import { inferAdditionalFields } from "better-auth/client/plugins";
 import { createAuthClient } from "better-auth/react";
 
 export const authClient = createAuthClient({
-  baseURL: "https://skill-bridge-six-liart.vercel.app/api",
+  baseURL: "https://skill-bridge-six-liart.vercel.app/api/auth",
   fetchOptions: {
     credentials: "include",
   },
